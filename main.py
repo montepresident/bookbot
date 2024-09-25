6acc4e50b2db821ab519667dba972c93cb1d93e8
montepresident <neilchristianmontemayor@gmail.com>
2024-09-24 20:23:12 -0700

    First day update before sleep

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+
+    def sum_words(start, stop):
+        for range(end)
+        book_string = string.split()
+        return book_string.index()
+
+main()
