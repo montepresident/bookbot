ca2fd0a7d4c0858bf640ed169c4ca39f078c9011
montepresident <neilchristianmontemayor@gmail.com>
2024-10-20 17:11:18 -0700

    Redid logic

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
-    def sum_words(start, stop):
-        book_string = []    
-        for words in file_contents(start, stop):
-            book_string.append(words)
-        return book_string.index()
+    def sum_words(text):
+        words = text.split()
+        return len(words)
+
+    word_count = sum_words(file_contents)
+    print(f"The book contains {word_count} words.")
 
 main()
