2aef1264f1595fe9dce3278ad0e266d051faf684
montepresident <neilchristianmontemayor@gmail.com>
2024-12-04 18:29:01 -0800

    Fixed bug in sum_words and added logic to frequency

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ def main():
         file_contents = f.read()
 
     def sum_words(text):
-        words = text.split()
-        return len(words)
+        return len(text.split())
 
     word_count = sum_words(file_contents)
     print(f"The book contains {word_count} words.")
 
+    def frequency(text):
+        dictionary = {}
+        for words in text.lower():
+            for char in words:
+                if char == words:
+                    dictionary[text] += 1
+        return dictionary
+    characters = frequency(file_contents)
+    print(f"Here are the details about the characters: {characters}")
+    
 main()
